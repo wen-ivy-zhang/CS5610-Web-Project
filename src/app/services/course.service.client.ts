a23a027f069e00600337d61f7c0cd17ed3f020c0
sunnyfz309 <sunnyfz309@gmail.com>
2019-04-19 15:54:18 -0700

    change env to prod

--- src/app/services/course.service.client.ts
+++ src/app/services/course.service.client.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { environment } from '../../environments/environment';
-// import {environment} from '../../environments/environment.prod';
-import { Course } from '../models/course.model.client';
+import {environment} from '../../environments/environment.prod';
+// import { Course } from '../models/course.model.client';
 import { map } from 'rxjs/operators';
 
 @Injectable()
